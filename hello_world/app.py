980326ea90a5b485c528cfb2fa5220c9938b3896
Erwin Debusschere <erwincdl@gmail.com>
2019-10-04 15:59:52 -0300

    Adds local context and refactors scrapper detection

--- hello_world/app.py
+++ hello_world/app.py
@@ -6,7 +6,9 @@ import os
 from botocore.exceptions import ClientError
 
 URLS = os.getenv('URLS_TABLE')
-urls_table = boto3.resource('dynamodb').Table(URLS)
+DYNAMO_ENDPOINT_URL = os.getenv('DB_ENDPOINT')
+REGION_NAME = os.getenv('REGION_NAME')
+urls_table = boto3.resource('dynamodb', region_name=REGION_NAME, endpoint_url=DYNAMO_ENDPOINT_URL).Table(URLS)
 
 
 class DecimalEncoder(json.JSONEncoder):
@@ -21,11 +23,10 @@ class DecimalEncoder(json.JSONEncoder):
 
 def lambda_handler(event, context):
     user_agent = event['headers']['User-Agent'].lower()
-    print(user_agent)
     slug = event['pathParameters']['slug']
     if slug is None:
         return response(400, {"error": 'slug is missing'})
-    if "bot" in user_agent or "facebookexternalhit" in user_agent:
+    if any(word in user_agent for word in ["bot", "facebookexternalhit"]):
         print("detected scrapping")
         return bot_landing()
     try:
